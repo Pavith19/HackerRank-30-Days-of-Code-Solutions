249404c93d5a68d70447e3200b1ad803d933ed65
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-19 17:11:16 +0530

    Update Solution.java

--- Day 02_ Operators/Solution.java
+++ Day 02_ Operators/Solution.java
@@ -1,1 +1,37 @@
+import java.io.*;
 
+class Result {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function accepts following parameters:
+     *  1. DOUBLE meal_cost
+     *  2. INTEGER tip_percent
+     *  3. INTEGER tax_percent
+     */
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        double tip = (meal_cost / 100) * tip_percent;
+        double tax = ((double) tax_percent / 100) * meal_cost;
+        System.out.println(Math.round(meal_cost + tip + tax));
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
+}
