7fe5688f7ec3c6e7ee1d5ec4cc4ac7bb0da95b63
Pavith Dinusara Bambaravanage <160596797+Pavith19@users.noreply.github.com>
2024-07-19 17:36:30 +0530

    Create Solution.java

---
+++ Day 29_ Bitwise AND/Solution.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.stream.*;
+
+class Result {
+
+    /*
+     * Complete the 'bitwiseAnd' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. INTEGER K
+     */
+
+    public static int bitwiseAnd(int N, int K) {
+        int max = 0;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 2; j <= N; j++) {
+                if (i == j) continue;
+                int bitwise = i & j;
+                if (bitwise < K && bitwise > max) {
+                    max = bitwise;
+                }
+            }
+        }
+        return max;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int count = Integer.parseInt(firstMultipleInput[0]);
+
+                int lim = Integer.parseInt(firstMultipleInput[1]);
+
+                int res = Result.bitwiseAnd(count, lim);
+
+                bufferedWriter.write(String.valueOf(res));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
